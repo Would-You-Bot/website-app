df8aa1ef34adacd959e60376bc9df2bddcc16b71
dpaulos6 <itzframepvp@outlook.com>
2024-04-11 12:07:17 +0100

    Legal Pages

---
+++ app/legal/page.tsx
@@ -0,0 +1,93 @@
+import Link from 'next/link'
+
+export default function legalnotice() {
+  return (
+    <main className="flex flex-col gap-8 px-8 text-neutral-300 xl:px-[17vw]">
+      <h1 className="mt-36 text-4xl font-bold text-brand-red-100 drop-shadow-red-glow">
+        Legal Notice
+      </h1>
+      <p>
+        <span className="mr-1.5 text-lg">🇩🇪</span>
+        For the German version please visit{' '}
+        <Link href="/legal-de" className="text-white underline">
+          this page
+        </Link>
+        .
+      </p>
+      <p>Information according to § 5 TMG.</p>
+      <div>
+        <h3 className="text-lg font-bold text-white">Contact</h3>
+        <p className="select-none">
+          Dominik Koch
+          <br />
+          Parkstrasse 5<br />
+          88499 Riedlingen
+          <br />
+          Germany
+        </p>
+      </div>
+      <p>No acceptance of parcels or packages.</p>
+      <p>Email: dominik@wouldyoubot.com</p>
+      <div>
+        <h3 className="text-lg font-bold text-white">
+          Online dispute resolution
+        </h3>
+        <p>
+          The European Comission provides a platform for online dispute
+          resolution, available at{' '}
+          <Link
+            href="https://ec.europa.eu/consumers/odr/"
+            className="text-white underline"
+          >
+            https://ec.europa.eu/consumers/odr/
+          </Link>
+          . <br />I am neither willing nor obliged to participate in dispute
+          resolution proceedings in front of a consumer arbitration board.
+        </p>
+      </div>
+      <div>
+        <h3 className="text-lg font-bold text-white">Privacy Policy</h3>
+        <Link
+          href="https://wouldyoubot.gg/privacy/"
+          className="text-white underline"
+        >
+          https://wouldyoubot.gg/privacy/
+        </Link>
+      </div>
+      <div>
+        <h3 className="text-lg font-bold text-white">
+          Validity of this Legal Notice
+        </h3>
+        <p>
+          This legal notice is valid for the following websites, social media
+          accounts and other services, as long as they are listed below.
+        </p>
+      </div>
+      <div className="text-white">
+        <Link href="https://wouldyoubot.com/" className="underline">
+          https://wouldyoubot.com/
+        </Link>
+        <br />
+        <Link href="https://wouldyoubot.gg/" className="underline">
+          https://wouldyoubot.gg/
+        </Link>
+        <br />
+
+        <Link href="https://rivo.gg/" className="underline">
+          https://rivo.gg/
+        </Link>
+        <br />
+        <Link href="https://twitter.com/WouldYouBot/" className="underline">
+          https://twitter.com/WouldYouBot/
+        </Link>
+      </div>
+      <p>
+        The Discord bot with the id{' '}
+        <span className="font-mono text-white">981649513427111957</span>
+        <br />
+        The Discord server with the id{' '}
+        <span className="font-mono text-white">1009562516105461780</span>
+      </p>
+    </main>
+  )
+}
---
+++ app/privacy/page.tsx
@@ -0,0 +1,395 @@
+import Link from 'next/link'
+
+export default function Privacy() {
+  return (
+    <main className="flex flex-col gap-8 px-8 text-neutral-300 xl:px-[17vw]">
+      <h1 className="mt-36 text-4xl font-bold text-brand-red-100 drop-shadow-red-glow">
+        Privacy Policy
+      </h1>
+      <div>
+        <h3 className="mb-4 text-lg font-bold text-white">
+          Last updated and effective March 08, 2024
+        </h3>
+        <p>
+          Thank you for choosing to be part of our community at Would
+          You(&quot;we,&quot; &quot;us,&quot; or &quot;our&quot;). We are
+          committed to protecting your personal information and your right to
+          privacy. If you have any questions or concerns about this privacy
+          notice or our practices with regard to your personal information,
+          please contact us at dominik@wouldyoubot.com.
+          <br />
+          <br />
+          This privacy notice describes how we might use your information if
+          you:
+        </p>
+        <ul>
+          <li>
+            - Visit our website at{' '}
+            <Link
+              href="https://wouldyoubot.gg"
+              target={'_blank'}
+              className="text-white underline"
+            >
+              https://wouldyoubot.gg
+            </Link>{' '}
+            or{' '}
+            <Link
+              href="https://wouldyoubot.com"
+              target={'_blank'}
+              className="text-white underline"
+            >
+              https://wouldyoubot.com
+            </Link>
+          </li>
+          <li>
+            - Engage with us in other related ways — including any sales,
+            marketing, or events.
+          </li>
+        </ul>
+        <p>
+          <br />
+          In this privacy notice, if we refer to:
+        </p>
+        <ul>
+          <li>
+            - &quot;Website,&quot; we are referring to any website of ours that
+            references or links to this policy.
+          </li>
+          <li>
+            - &quot;Services,&quot; we are referring to our Website, and other
+            related services, including any sales, marketing, or events.
+          </li>
+        </ul>
+        <p>
+          <br />
+          The purpose of this privacy notice is to explain to you in the
+          clearest way possible what information we collect, how we use it, and
+          what rights you have in relation to it. If there are any terms in this
+          privacy notice that you do not agree with, please discontinue use of
+          our Services immediately.
+        </p>
+
+        <h3 className="my-4 text-lg font-bold text-white">Privacy Statement</h3>
+        <p>
+          We respect the privacy of your information. We provide this
+          explanation about our information practices as a show of our
+          commitment to protect your privacy. This policy describes the types of
+          information we may collect from you or that you may provide when you
+          visit this website and our practices for collecting, using,
+          maintaining, protecting and disclosing that information.
+          <br />
+          <br />
+          This policy applies to information we collect:
+          <br />
+          <br />
+          On this website and/or via our Bot:
+        </p>
+        <ul>
+          <li>
+            - Your discord user id, avatar and custom questions uploaded to our
+            marketplace by the user.
+          </li>
+          <li>- Questions you liked or shared on our marketplace.</li>
+          <li>
+            - Data about your discord server including your server id, avatar,
+            settings, webhook urls, custom uploaded messages and
+          </li>
+          <li>
+            - If and when available, through mobile or desktop applications you
+            download from this website, which provide dedicated non-browser-
+            based interaction between you and this website.
+          </li>
+          <li>
+            - When you interact with our applications on social media or
+            third-party applications, websites and services, if those
+            applications or advertising including links to this policy or of
+            information from social media or third parties.
+          </li>
+        </ul>
+        <p>
+          <br />
+          It does not apply to information collected by:
+        </p>
+        <ul>
+          <li>
+            - us offline or through any other means, including on any other
+            website operated by Would You or any third party;
+          </li>
+          <p>or</p>
+          <li>
+            - any third party, including through any application or content
+            (including advertising) that may link to or be accessible from or on
+            the website.
+          </li>
+        </ul>
+        <p>
+          <br />
+          Please read this policy carefully to understand our policies and
+          practices regarding your information and how we treat it. If you do
+          not agree with our policies and practices, your choice is not to use
+          our website or bot. By accessing or using this website/bot, you agree
+          to this privacy policy. This policy may change from time to time. Your
+          continued use of this website after we make changes is deemed to be
+          acceptance of those changes, so please check the policy every once in
+          a while for updates.
+        </p>
+        <h3 className="my-4 text-lg font-bold text-white">
+          {' '}
+          What are your Privacy rights?
+        </h3>
+        <p>
+          In some regions (like the PEA and UK), you have certain rights under
+          applicable data protection laws. These may include the right (i) to
+          request access and obtain a copy of your personal information, (ii) to
+          request rectification or erasure; (Hi) to restrict the processing of
+          your personal information; and (iv) If applicable, to data
+          portability. In certain circumstances, you may also have the right to
+          object to the processing of your personal Inform. To make such a
+          request please use the contact details provided below. We will
+          consider and act upon any request in accordance with applicable data
+          protection laws. If we are relying on your consent to process your
+          personal information, you have the right to withdraw your consent at
+          any time. Please note however that this will not affect the lawfulness
+          of the processing before its withdrawal, nor will it affect the
+          processing of your personal information conducted in reliance on
+          lawful processing grounds other than consent. If you are a resident in
+          the EEA or UK and you believe we are unlawfully processing your
+          personal Information, you also have the right to complain to your
+          local data protection supervisory authority. You can find their
+          contact details here:{' '}
+          <Link
+            href="https://ec.europa.eugustice/data-protectun/bodies/authontlesfindex_en.htm"
+            target={'_blank'}
+            className="text-white underline"
+          >
+            https://ec.europa.eugustice/data-protectun/bodies/authontlesfindex_en.htm
+          </Link>
+          . If you are a resident in Switzerland, the contact details for the
+          data protection authorities are available here:{' '}
+          <Link
+            href="https://www.edoeb.admin.ch/edoebtenfhome.html"
+            target={'_blank'}
+            className="text-white underline"
+          >
+            https://www.edoeb.admin.ch/edoebtenfhome.html
+          </Link>
+          .
+        </p>
+
+        <h3 className="my-4 text-lg font-bold text-white"> Age Limit</h3>
+        <p>
+          {' '}
+          The Bot is not intended for use by individuals under the age of 13.
+          The Creator does not knowingly collect personal information from
+          children under 13 years of age. If you are a parent or guardian and
+          believe that your child has provided personal information to the Bot,
+          please contact the Creator to request deletion of that information.
+        </p>
+
+        <h3 className="my-4 text-lg font-bold text-white">
+          Analytics Provided by Others
+        </h3>
+        <p>
+          Our website uses Plausible Analytics to help us understand visitor
+          trends and the effectiveness of our marketing outreach. We chose
+          Plausible Analytics because it is a privacy-focused company and
+          platform that eschews personally identifiable information in favor of
+          anonymous aggregate data. We do not use Plausible Analytics to track
+          or collect any personally identifiable information about you.
+          Plausible is self-hosted and does not use cookies nor store any
+          personal information.{' '}
+          <Link
+            href="https://plausible.io/data-policy"
+            target={'_blank'}
+            className="text-white underline"
+          >
+            See the Plausible Analytics Data Policy.
+          </Link>
+        </p>
+
+        <h3 className="my-4 text-lg font-bold text-white">
+          Information We Collect and How We Collect It
+        </h3>
+        <p>
+          Personally Identifiable Information. On our website, we only collect
+          personally identifiable information from individuals that they provide
+          to us voluntarily. This means we do not require you to register or
+          provide information to us in order to view our website. Would You only
+          gathers personally identifiable data through our website, such as an
+          email address, when voluntarily submitted by a visitor. Once
+          collected, we may combine this information with other information
+          collected from external sources.
+          <br />
+          <br />
+        </p>
+        <ul>
+          <li>
+            <Link
+              href="https://en.wikipedia.org/wiki/HTTP_cookie"
+              target={'_blank'}
+              className="text-white underline"
+            >
+              Cookies (or browser cookies)
+            </Link>
+            . You may refuse to accept browser cookies by activating the
+            appropriate setting on your browser. However, if you select this
+            setting you may be unable to access certain parts of our website.
+            Unless you have adjusted your browser setting so that it will refuse
+            cookies, our system will issue cookies when you direct your browser
+            to our website.
+          </li>
+        </ul>
+        <h3 className="font-lg my-4 font-bold text-white">
+          Third-Party Websites
+        </h3>
+        <p>
+          This website or messages distributed by our bot may contain links to
+          third-party websites. These linked websites are not under our control,
+          and we are not responsible for the privacy practices or the contents
+          of any such linked website or any link contained in any linked
+          website. We provide such links only as a convenience, and the
+          inclusion of a link on the website does not imply endorsement of the
+          linked website by Would You. If you provide any personal data through
+          any such third-party website, your transaction will occur on the third
+          party&apos;s website (not this website) and the personal data you
+          provide will be collected by and controlled by the privacy policy of
+          that third party. We recommend that you familiarize yourself with the
+          privacy policies and practices of any third parties. Please note that
+          this privacy policy does not address the privacy or Information
+          practices of any third party website or tool.
+        </p>
+
+        <h3 className="my-4 text-lg font-bold text-white">
+          The Way We Use Information
+        </h3>
+        <p>
+          This Privacy Statement governs use of the information that you provide
+          to us through the website or bot. It does not govern the manner in
+          which we may use information obtained from other sources (such as
+          public records) or from you by means other than this website. We and
+          other entities that we are involved in, or provide support for, the
+          operation of this website or other purposes use information that we
+          collect about you or that you provide to us, including any personal
+          information:
+        </p>
+        <ul>
+          <li>- To present our website and its contents to you.</li>
+          <li>
+            - To provide you with information on programs, services or products.
+          </li>
+          <li>
+            - For you to register for events, promotions, contests, sweepstakes
+            or services.
+          </li>
+          <li>
+            - To process online transactions, purchases, registrations,
+            donations or participation in programs.
+          </li>
+          <li>
+            - To provide you with information that may be targeted to your
+            interests.
+          </li>
+          <li>- To fulfill any other purpose for which you provide it.</li>
+          <li>
+            - To carry out our obligations and enforce our rights arising from
+            any contracts entered into between you and us.
+          </li>
+          <li>
+            - To request additional information from you for various purposes.
+          </li>
+          <li>
+            - In any other way we may describe when you provide the information.
+          </li>
+          <li>
+            - For any other purpose with your consent. We may also use your
+            information if provided to us to contact you about our own and
+            third-party services and products that may be of interest to you. If
+            you do not want us to use your information in this way, you can opt
+            out of receiving this information by so indicating in your
+            registration/preferences file or by notifying us.
+          </li>
+        </ul>
+        <p>
+          <br />
+          We may also use or disclose information to resolve disputes,
+          investigate problems or enforce our{' '}
+          <Link
+            href="/terms"
+            target={'_blank'}
+            className="text-white underline"
+          >
+            Terms of Service
+          </Link>
+          . At times, we may review status or activity of multiple users to do
+          so. We may disclose or access information whenever we believe in good
+          faith that the law so requires or if we otherwise consider it
+          necessary to do so to maintain service and improve our services. We
+          use your IP address to help diagnose problems with our server, to
+          manage our website and to enhance our site based on the usage pattern
+          we receive.
+        </p>
+        <h3> Do California residents have specific privacy rights?</h3>
+        <p>
+          California Civil Code Section 1798.83, also known as the &quot;Shine
+          The Light&quot; law, permits our users who are California residents to
+          request and obtain from us, once a year and free of charge,
+          information about categories of personal information (if any) we
+          disclosed to third parties for direct marketing purposes and the names
+          and addresses of all third parties with which we shared personal
+          Information in the Immediately preceding calendar year. If you are a
+          California resident and would like to make such a request, please
+          submit your request in writing to us using the contact Information
+          provided below. If you are under 18 years of age, reside in
+          California, and have a registered account with the Website, you have
+          the right to request removal of unwanted data that you publicly post
+          on the Website. To request removal of such data, please contact us
+          using the contact information provided below, and include the email
+          address associated with your account and a statement that you reside
+          In California. We will make sure the data is not publicly displayed on
+          the Website, but please be aware that the data may not be completely
+          or comprehensively removed from all our systems (e.g. backups etc.).
+        </p>
+        <h3 className="my-4 text-lg font-bold text-white">
+          {' '}
+          Do we make updates to this notice?
+        </h3>
+        <p>
+          We may update this privacy notice from time to time. The updated
+          version will be indicated by an updated &quot;Revised&quot; date and
+          the updated version will be effective as soon as it is accessible. If
+          we make material changes to this privacy notice, we may notify you
+          either by prominently posting a notice of such changes or by directly
+          sending you a notification. We encourage you to review this privacy
+          notice frequently to be informed of how we are protecting your
+          information.
+        </p>
+        <h3 className="my-4 text-lg font-bold text-white">
+          {' '}
+          How can you contact us about this notice?
+        </h3>
+        <p>
+          If you have any questions or comments about this notice, you may email
+          us at dominik@wouldyoubot.com.
+        </p>
+        <h3 className="my-4 text-lg font-bold text-white">
+          How can you review, update, or delete the data we collect from you?
+        </h3>
+        <p>
+          Based on the applicable laws of your country, you may have the right
+          to request access to the personal information we collect from you,
+          change that information, or delete it in some circumstances. To
+          request to review, update, or delete your personal Information, please
+          visit:{' '}
+          <Link
+            href="/discord"
+            target={'_blank'}
+            className="text-white underline"
+          >
+            https://wouldyoubot.gg/discord
+          </Link>
+          .
+        </p>
+      </div>
+    </main>
+  )
+}
