feb9fcd1957db8ad4b555509ede38218c2a062b4
Dominik Koch <dominik@koch-bautechnik.de>
2024-04-12 16:14:26 +0200

    add schema component

---
+++ components/Schema.tsx
@@ -0,0 +1,8 @@
+export function Schema({json}: { json: string }): JSX.Element {
+    return (
+        <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: json }}
+        />
+    );
+}
