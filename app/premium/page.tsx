0e4e1c7f18b7091f1cd657664e77fd4175149c6c
Dominik Koch <dominik@koch-bautechnik.de>
2024-04-21 19:50:09 +0200

    add old premium code

---
+++ app/premium/page.tsx
@@ -0,0 +1,165 @@
+// TODO remove the use client directive in favor of a server component
+"use client";
+
+import { useState } from "react";
+import Head from "next/head";
+import { LazyMotion, domAnimation, m } from "framer-motion";
+import Button from "@/components/Button";
+
+const CheckArrowIcon = () => (
+  <div className="mr-4 flex h-5 w-5 items-center justify-center rounded-full bg-transparent">
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      className="text-customPrimary"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.383 4.117a1.252 1.252 0 0 1 0 1.77l-10 10a1.252 1.252 0 0 1-1.77 0l-5-5a1.252 1.252 0 0 1 1.77-1.77L7.5 13.23l9.117-9.113a1.252 1.252 0 0 1 1.77 0h-.004Z"
+        fill="currentColor"
+      />
+    </svg>
+  </div>
+);
+const XIcon = () => (
+  <div className="mr-4 flex h-5 w-5 items-center justify-center rounded-full bg-transparent">
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      className="text-[#5A5B5E]"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.313 1 9.749 7.563 3.187 1 1 3.188 7.562 9.75 1 16.313 3.187 18.5l6.562-6.563 6.564 6.563 2.187-2.188-6.562-6.562L18.5 3.187 16.313 1Z"
+        fill="currentColor"
+      />
+    </svg>
+  </div>
+);
+type PricingData = {
+  price: { monthly: number; yearly: number };
+  premium: {
+    "All Freemium Features": boolean;
+    "Unlimited Custom Questions": boolean;
+    "Customized Webhook Branding": boolean;
+    "No More Invite Button Ads": boolean;
+    "Auto Pin Daily Messages": boolean;
+    "AI-Generated Questions": boolean;
+  };
+};
+
+const pricingData: PricingData = {
+  price: { monthly: 2.99, yearly: 29.99 },
+  premium: {
+    "All Freemium Features": true,
+    "Unlimited Custom Questions": true,
+    "Customized Webhook Branding": true,
+    "No More Invite Button Ads": true,
+    "Auto Pin Daily Messages": true,
+    "AI-Generated Questions": false,
+  },
+};
+
+export default function Premium() {
+  const [isMonthly, setIsMonthly] = useState(true);
+  const [isPremiumOpen, setIsPremiumOpen] = useState(false);
+
+  const handleChange = () => {
+    setIsMonthly(!isMonthly);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Would You - Commands</title>
+      </Head>
+      <main className="relative flex w-screen justify-center bg-customDarkBg2 px-8 xl:px-[17vw]">
+        <div className="bg-customDarkBg2 ">
+          <LazyMotion features={domAnimation}>
+            <m.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+            >
+              <div className="container mx-auto px-4">
+                <h1 className="mt-36 text-4xl font-bold text-yellow-500 drop-shadow-gold-glow">
+                  Premium
+                </h1>
+                <p className="mb-6 text-customGrayText">
+                  Select the plan that suits your needs and benefit from our
+                  discord bot.
+                </p>
+                <div className="mx-auto max-w-2xl text-center">
+                  <label className="group relative mx-auto flex h-16 w-52 cursor-pointer items-center justify-between rounded-lg bg-customDarkBg3 pl-2 pr-36 text-xl">
+                    <input
+                      type="checkbox"
+                      className="peer appearance-none"
+                      checked={!isMonthly}
+                      onChange={handleChange}
+                    />
+                    <span className="flex h-16 w-[6rem] cursor-pointer items-center  duration-300 ease-in-out after:h-12 after:w-[20rem] after:rounded-lg after:bg-customPrimary after:shadow-md after:duration-300 peer-checked:after:translate-x-[6rem]"></span>
+                    <div className="absolute flex text-base font-bold text-white">
+                      <div
+                        className={
+                          isMonthly ? "ml-4 mr-10" : "ml-4 mr-10 text-gray-400"
+                        }
+                      >
+                        Monthly
+                      </div>
+                      <div className={(isMonthly && "text-gray-400") as string}>
+                        Yearly
+                      </div>
+                    </div>
+                  </label>
+                </div>
+                <div className="mb-8 w-[350px] rounded-[1.75rem] bg-gradient-premium p-1 sm:w-[380px] lg:mb-0 lg:w-1/3">
+                  <div className="rounded-3xl bg-customDarkBg3 px-8 py-8 ">
+                    <h4 className="font-heading mb-2 text-left text-2xl font-bold text-white 2xl:mb-4">
+                      Premium
+                    </h4>
+                    <div className="flex items-end justify-start">
+                      <div className="mr-2 mt-4 text-left text-4xl font-bold text-white sm:text-5xl">
+                        {isMonthly ? pricingData.price.monthly : pricingData.price.yearly}
+                      </div>
+                      <div className="text-gray-500">
+                        {isMonthly ? "/ month" : "/ year"}
+                      </div>
+                    </div>
+                    <p className="mb-8 mt-8 text-left leading-loose text-gray-500 2xl:mb-12">
+                      Experience the full power of our Would You bot.
+                    </p>
+                    <ul className="mb-14 text-white">
+                      {Object.keys(pricingData["premium"]).map(
+                        (text, index) => (
+                          <li className="mb-4 flex" key={`${text}-${index}`}>
+                            {pricingData["premium"][
+                              text as keyof (typeof pricingData)["premium"]
+                            ] ? (
+                              <CheckArrowIcon />
+                            ) : (
+                              <XIcon />
+                            )}
+                            <span>{text}</span>
+                          </li>
+                        )
+                      )}
+                    </ul>
+                    <Button
+                      className=" mt-20 w-full justify-center rounded-xl rounded-t-xl py-2 font-bold leading-loose"
+                      onClick={() => setIsPremiumOpen(true)}
+                    >
+                      Get Started
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </m.div>
+          </LazyMotion>
+        </div>
+      </main>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,18 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          customPrimary: "rgb(5, 152, 244)",
+          customSecondary: "rgb(5, 152, 244)",
+          customDarkBg1: "rgb(3, 122, 199)",
+          customDarkBg2: "rgb(23, 23, 23)",
+          customDarkBg3: "rgb(20, 20, 20)",
+          customDarkBg3Hover: "rgb(55, 56, 62)",
+          customContentSubtitle: "rgb(178, 184, 205)",
+          customGrayBorder: "rgb(255,255,255,0.1)",
+          customGrayText: "rgb(161, 161, 161)",
+          customDarkBgTransparent: "rgb(31, 32, 35, 0.7)",
+          customDarkBgTransparentDarker: "rgb(0,0,0,0.5)",
+          customDarkBgTransparentLighter: "rgb(48, 49, 54, 0.7)",
           blue: { 100: "#0598F4", 200: "#037ac7", 300: "#016cb0" },
           red: { 100: "#F00605", 200: "#b60505", 300: "#9a0404" },
         },
@@ -17,9 +29,12 @@ module.exports = {
       dropShadow: {
         ["red-glow"]: ["2px 2px 10px #f00505"],
         ["blue-glow"]: ["2px 2px 10px #0598f6"],
+        ["gold-glow"]: ["2px 2px 10px #eab308"],
       },
     },
     backgroundImage: {
+      ["gradient-premium"]:
+      "linear-gradient(to bottom right, #0598F4 0%,#F00605 100%)",
       ["gradient-brand"]:
         "linear-gradient(92.91deg, #0598F4 -50%,#F00605 150%)",
     },
