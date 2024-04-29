e0b9a9295cf5e0e64e57b8c13b6b02423de25f65
Jester0027 <paul.ned@outlook.com>
2024-04-29 19:59:56 +0200

    feat: Added auth token helpers

--- helpers/oauth/helpers.ts
+++ helpers/oauth/helpers.ts
@@ -1,8 +1,41 @@
 import { IdTokenJWT } from "@/helpers/hooks";
 import { cookies } from "next/headers";
 import { parseJWT } from "oslo/jwt";
+import { OAuthTokenJWT } from "@/helpers/oauth/types";
+import { verifyJwt } from "@/helpers/jwt";
+import { cache } from "react";
 
 export function getIdToken(): IdTokenJWT | null {
   const tokenString = cookies().get("ID_TOKEN")?.value;
   return tokenString ? (parseJWT(tokenString) as IdTokenJWT) : null;
 }
+
+/**
+ * @desc Parses the current auth token as a JWT string and validates it,
+ * including the signature, expiration, and not-before date.
+ * Throws if the JWT is invalid or expired.
+ */
+export function getAuthToken(): Promise<OAuthTokenJWT> {
+  const token = cookies().get("OAUTH_TOKEN")?.value;
+
+  if (!token) {
+    throw new MissingTokenException();
+  }
+
+  return verifyJwt(token) as Promise<OAuthTokenJWT>;
+}
+
+/**
+ * @desc Returns the validated auth token, or null if any error is thrown (e.g. not token found, token is expired, invalid signature,...)
+ */
+export const getAuthTokenOrNull = cache((): Promise<OAuthTokenJWT | null> => {
+  try {
+    return getAuthToken();
+  } catch (e: unknown) {
+    return Promise.resolve(null);
+  }
+});
+
+export abstract class OAuthException extends Error {}
+
+export class MissingTokenException extends OAuthException {}
---
+++ helpers/oauth/types.ts
@@ -0,0 +1,22 @@
+import { JWT } from "oslo/jwt";
+
+export type IdTokenData = {
+  id: string;
+  avatar: string;
+  username: string;
+  global_name: string;
+  exp: number;
+};
+
+export interface IdTokenJWT extends JWT {
+  payload: IdTokenData;
+}
+
+export interface OAuthTokenData extends IdTokenData {
+  access_token: string;
+  refresh_token: string;
+}
+
+export interface OAuthTokenJWT extends JWT {
+  payload: OAuthTokenData;
+}
