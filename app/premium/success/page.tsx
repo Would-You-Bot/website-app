7096235647aff88884dcf02ce17bec09ab2ab875
dpaulos6 <itzframepvp@outlook.com>
2024-05-15 10:59:45 +0100

    Sucess Page Fixes

--- app/premium/success/page.tsx
+++ app/premium/success/page.tsx
@@ -2,31 +2,43 @@
 "use client";
 import Button from "@/components/Button";
 import Head from "next/head";
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
 
 // To test the searchParams use http://localhost:3000/premium/success?type=monthly&server=942897714956472401
 
 // TODO: Add confetti animation on page and button click!
 export default function Premium() {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
 
-  const type = searchParams.get("type")
-  const serverId = searchParams.get("server")
+  const type = searchParams.get("type");
+  const serverId = searchParams.get("server");
 
   return (
     <>
       <Head>
         <title>Would You - Commands</title>
       </Head>
-      <main className="flex flex-col gap-8 px-8 text-neutral-300">
+      <main className="flex flex-col flex-1 items-center justify-center gap-8 px-8 text-neutral-300">
         <h1 className="mt-36 text-4xl font-bold text-brand-blue-100 text-center drop-shadow-blue-glow">
           Payment Successful!
         </h1>
         <p className="mb-4 text-lg text-white">
-          Your payment for Would You Premium billed <b>{type === "monthly" ? "monthly" : "annually"}</b> was successful!
+          Your payment for Would You Premium billed{" "}
+          <b>{type === "monthly" ? "monthly" : "annually"}</b> was successful!
         </p>
-        // TODO: Add space between the premium text
-        <Button className="justify-center" onClick={() => window.open(`https://discord.com/channels/${serverId}/`, "_blank")}>Click to visit your new <b>Premium</b> server</Button>
+        {
+          // TODO: Add space between the premium text
+        }
+        <Button
+          className="w-fit"
+          onClick={() =>
+            window.open(`https://discord.com/channels/${serverId}/`, "_blank")
+          }
+        >
+          <span>
+            Click to visit your new <b>Premium</b> server
+          </span>
+        </Button>
       </main>
     </>
   );
