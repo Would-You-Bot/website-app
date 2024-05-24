815666f55e4bb3ec5208a43243e7530187e02d99
Jester0027 <paul.ned@outlook.com>
2024-05-24 22:55:55 +0200

    feat(question-packs): Adjust colors

--- app/question-packs/_components/PopularTag.tsx
+++ app/question-packs/_components/PopularTag.tsx
@@ -6,7 +6,7 @@ export async function PopularTag() {
   return (
     <div
       aria-hidden
-      className="px-2 py-3 h-[23px] text-xs font-[400] flex items-center justify-center gap-1 bg-gradient-tag rounded-[7px] uppercase"
+      className="px-2 py-3 h-[23px] text-xs font-[400] flex items-center justify-center gap-1 bg-gradient-tag rounded-[7px] uppercase text-white"
     >
       <FireIcon className="w-4 h-4" /> POPULAR
     </div>
--- app/question-packs/_components/QuestionPackCard.tsx
+++ app/question-packs/_components/QuestionPackCard.tsx
@@ -18,29 +18,27 @@ export async function QuestionPackCard({ pack }: { pack: QuestionPack }) {
     >
       <div className="p-4 bg-[#141414] rounded-[10px]">
         <div className="flex justify-between mb-2">
-          <p className="font-bold text-2xl text-neutral-100">{pack.title}</p>
+          <p className="font-bold text-2xl text-white">{pack.title}</p>
           {isPopular && <PopularTag />}
         </div>
-        <p>{pack.description}</p>
+        <p className="text-[#B4B4B4]">{pack.description}</p>
         <div className="grid grid-cols-2 gap-2 mt-4">
           <div>
-            <p>Questions</p>
-            <p className="text-neutral-100 font-bold">
-              {pack.numberOfQuestions}
-            </p>
+            <p className="text-[#898989]">Questions</p>
+            <p className="text-white font-bold">{pack.numberOfQuestions}</p>
           </div>
           <div>
-            <p>Type</p>
-            <p className="text-neutral-100 font-bold">{pack.type}</p>
+            <p className="text-[#898989]">Type</p>
+            <p className="text-white font-bold">{pack.type}</p>
           </div>
         </div>
         <div className="grid grid-cols-2 gap-2 mt-4">
-          <button className="bg-[#101010] p-3 rounded-lg flex items-center justify-center gap-2">
+          <button className="bg-[#101010] p-3 rounded-lg flex items-center justify-center gap-2 text-[#909090]">
             <HeartIcon /> {pack.numberOfLikes} Likes
           </button>
           <button
             className={cn(
-              "rounded-lg flex items-center justify-center gap-2",
+              "rounded-lg flex items-center justify-center gap-2 text-white",
               isPopular ? "bg-gradient-brand" : "bg-blue-500",
             )}
           >
