dd6d46de5c94f24744fac5e9e88ff83dae9f223b
dpaulos6 <itzframepvp@outlook.com>
2024-07-08 11:09:41 +0100

    missing files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   prefix: "",
   theme: {
@@ -16,11 +16,11 @@ const config = {
     extend: {
       screens: {
         xs: "480px",
-        "2xl": "1400px",
+        "2xl": "1400px"
       },
       maxWidth: {
         "8xl": "90rem",
-        "9xl": "100rem",
+        "9xl": "100rem"
       },
       colors: {
         brand: {
@@ -37,7 +37,7 @@ const config = {
           customDarkBgTransparentDarker: "rgb(0,0,0,0.5)",
           customDarkBgTransparentLighter: "rgb(48, 49, 54, 0.7)",
           blue: { 100: "#0598F4", 200: "#037ac7", 300: "#016cb0" },
-          red: { 100: "#F00605", 200: "#b60505", 300: "#9a0404" },
+          red: { 100: "#F00605", 200: "#b60505", 300: "#9a0404" }
         },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -46,66 +46,66 @@ const config = {
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          foreground: "hsl(var(--primary-foreground))"
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          foreground: "hsl(var(--secondary-foreground))"
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          foreground: "hsl(var(--destructive-foreground))"
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          foreground: "hsl(var(--muted-foreground))"
         },
         accent: {
           DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          foreground: "hsl(var(--accent-foreground))"
         },
         popover: {
           DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          foreground: "hsl(var(--popover-foreground))"
         },
         card: {
           DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+          foreground: "hsl(var(--card-foreground))"
+        }
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        sm: "calc(var(--radius) - 4px)"
       },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "var(--radix-accordion-content-height)" }
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+          to: { height: "0" }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out"
       },
       dropShadow: {
         ["red-glow"]: ["2px 2px 10px #f00505"],
         ["blue-glow"]: ["2px 2px 10px #0598f6"],
-        ["gold-glow"]: ["2px 2px 10px #eab308"],
+        ["gold-glow"]: ["2px 2px 10px #eab308"]
       },
       backgroundImage: {
         ["gradient-premium"]:
           "linear-gradient(to bottom right, #0598F4 0%,#F00605 100%)",
         ["gradient-brand"]:
-          "linear-gradient(92.91deg, #0598F4 -50%,#F00605 150%)",
-      },
-    },
+          "linear-gradient(92.91deg, #0598F4 -50%,#F00605 150%)"
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")]
+} satisfies Config
 
-export default config;
+export default config
