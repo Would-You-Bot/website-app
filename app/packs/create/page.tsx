62c914a0732d93b53c21a21d9bbf3522c0527f61
Diogo Paulos <itzframepvp@outlook.com>
2024-09-25 11:43:28 +0100

    feat: add text glow + focus state outline

--- app/packs/create/page.tsx
+++ app/packs/create/page.tsx
@@ -11,8 +11,8 @@ function Create() {
   return (
     <Container className="pt-8 lg:pt-10 space-y-8">
       <h1 className="space-x-2 text-xl lg:text-2xl font-bold xl:text-4xl">
-        <span className="text-brand-red-100">Create</span>
-        <span className="text-brand-blue-100">Pack</span>
+        <span className="text-brand-red-100 drop-shadow-red-glow">Create</span>
+        <span className="text-brand-blue-100 drop-shadow-blue-glow">Pack</span>
       </h1>
       <PackForm />
     </Container>
--- app/packs/page.tsx
+++ app/packs/page.tsx
@@ -11,8 +11,10 @@ function Packs() {
   return (
     <Container className="pt-8 lg:pt-10 space-y-8">
       <h1 className="space-x-2 text-xl lg:text-2xl font-bold xl:text-4xl">
-        <span className="text-brand-red-100">Question</span>
-        <span className="text-brand-blue-100">Packs</span>
+        <span className="text-brand-red-100 drop-shadow-red-glow">
+          Question
+        </span>
+        <span className="text-brand-blue-100 drop-shadow-blue-glow">Packs</span>
       </h1>
     </Container>
   )
