113acb7de5e5cc80cb5722b7d6690cf636e492a5
Dominik Koch <dominik@koch-bautechnik.de>
2024-11-04 19:21:18 +0100

    style: format code

--- lib/ratelimiter.ts
+++ lib/ratelimiter.ts
@@ -1,14 +1,14 @@
-import {Ratelimit} from "@upstash/ratelimit";
-import {redis} from "./redis";
+import { Ratelimit } from '@upstash/ratelimit'
+import { redis } from './redis'
 
 const defautlRateLimiter = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(50, "60 s")
+  limiter: Ratelimit.slidingWindow(50, '60 s')
 })
 
 const createRateLimiter = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(5, "60 s")
+  limiter: Ratelimit.slidingWindow(5, '60 s')
 })
 
-export {defautlRateLimiter, createRateLimiter}
+export { defautlRateLimiter, createRateLimiter }
