892429855b0cd3308f37602a46de7a9ce348e2e2
Dominik Koch <dominik@koch-bautechnik.de>
2024-11-06 19:59:01 +0100

    fix: return featured flag too

--- app/api/packs/route.ts
+++ app/api/packs/route.ts
@@ -24,6 +24,7 @@ export async function GET(request: NextRequest) {
     select: {
       type: true,
       id: true,
+      featured: true,
       name: true,
       description: true,
       tags: true,
