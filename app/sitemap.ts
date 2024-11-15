1829c67ae211fdcb691f331a72f5fa2776a6dfef
Dominik Koch <dominik@koch-bautechnik.de>
2024-11-15 15:37:34 +0100

    fix: add sitemap entry

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -109,5 +109,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: 'https://wouldyoubot.gg/blog/10-simple-fun-discord-event-ideas',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
   ]
 }
