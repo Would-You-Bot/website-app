4a445492a0fbb7934296a51c1a8f6f3041aedc15
Dominik Koch <dominik@koch-bautechnik.de>
2024-12-01 18:46:46 +0100

    feat(packs): add edit and delete api route

--- app/api/packs/[id]/route.ts
+++ app/api/packs/[id]/route.ts
@@ -1,42 +1,47 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse, type NextRequest } from "next/server";
-import validator from "validator";
+import { getAuthTokenOrNull } from '@/helpers/oauth/helpers'
+import { NextResponse, type NextRequest } from 'next/server'
+import { prisma } from '@/lib/prisma'
+import validator from 'validator'
 
 // Get all packs left to review we need request otherwise we can't get the params
-export async function GET(request: NextRequest, {
-  params,
-}: {
-  params: Promise<{ id: string }>
-}) {
+export async function GET(
+  request: NextRequest,
+  {
+    params
+  }: {
+    params: Promise<{ id: string }>
+  }
+) {
   const id = validator.escape((await params).id)
 
   const question = await prisma.questionPack
-  .findFirst({
-    where: {
-      id: id,
-      pending: false,
-      denied: false
-    },
-    select: {
-      type: true,
-      id: true,
-      language: true,
-      featured: true,
-      name: true,
-      description: true,
-      tags: true,
-      likes: true,
-      questions: true
-    },
-  }).catch(() => {
-    return NextResponse.json(
-      { message: 'Error fetching question!' },
-      { status: 500 }
-    )
-  })
+    .findFirst({
+      where: {
+        id: id,
+        pending: false,
+        denied: false
+      },
+      select: {
+        type: true,
+        id: true,
+        language: true,
+        featured: true,
+        name: true,
+        description: true,
+        tags: true,
+        likes: true,
+        questions: true
+      }
+    })
+    .catch(() => {
+      return NextResponse.json(
+        { message: 'Error fetching question!' },
+        { status: 500 }
+      )
+    })
 
-  // @ts-expect-error 
-  if(question.status === 500) {
+  // @ts-expect-error
+  if (question.status === 500) {
     return NextResponse.json(
       { message: 'Error getting that question, please contact the support!' },
       { status: 500 }
@@ -50,8 +55,131 @@ export async function GET(request: NextRequest, {
     )
   }
 
-  return NextResponse.json(
-    { data: question },
-    { status: 200 }
-  )
+  return NextResponse.json({ data: question }, { status: 200 })
+}
+
+export async function PATCH(
+  request: NextRequest,
+  {
+    params
+  }: {
+    params: Promise<{ id: string }>
+  }
+) {
+  const id = validator.escape((await params).id)
+  const auth = await getAuthTokenOrNull()
+  const userId = auth?.payload?.id
+
+  const pack = await prisma.questionPack.findFirst({
+    where: {
+      id: id,
+      pending: false,
+      denied: false
+    },
+    select: {
+      id: true,
+      name: true,
+      authorId: true,
+      description: true,
+      tags: true,
+      questions: true
+    }
+  })
+
+  if (!pack) {
+    return NextResponse.json(
+      { message: 'No question found with that id!' },
+      { status: 404 }
+    )
+  }
+
+  if (pack.authorId !== userId) {
+    return NextResponse.json(
+      { message: 'You are not the author of this question pack!' },
+      { status: 401 }
+    )
+  }
+
+  const data = await request.json()
+
+  const { editedPackSchema } = await import('@/utils/zod/schemas')
+
+  const parsedPackResult = editedPackSchema.safeParse(data)
+
+  if (!parsedPackResult.success) {
+    return NextResponse.json(
+      { message: parsedPackResult.error.issues },
+      { status: 400 }
+    )
+  }
+
+  const editPack = await prisma.questionPack.update({
+    where: {
+      id: pack.id
+    },
+    data: {
+      name: data.name,
+      description: data.description,
+      tags: data.tags,
+      questions: data.questions,
+      pending: true
+    }
+  })
+
+  if (!editPack) {
+    return NextResponse.json(
+      { message: 'Error updating pack!' },
+      { status: 500 }
+    )
+  }
+
+  return NextResponse.json({ message: 'Your question pack was updated!' }, { status: 200 })
+
+}
+
+export async function DELETE(
+  request: NextRequest,
+  {
+    params
+  }: {
+    params: Promise<{ id: string }>
+  }
+) {
+  const id = validator.escape((await params).id)
+  const auth = await getAuthTokenOrNull()
+  const userId = auth?.payload?.id
+
+  const pack = await prisma.questionPack.findFirst({
+    where: {
+      id: id,
+      pending: false,
+      denied: false
+    },
+    select: {
+      id: true,
+      authorId: true
+    }
+  })
+
+  if (!pack) {
+    return NextResponse.json(
+      { message: 'No question found with that id!' },
+      { status: 404 }
+    )
+  }
+
+  if (pack.authorId !== userId) {
+    return NextResponse.json(
+      { message: 'You are not the author of this question pack!' },
+      { status: 401 }
+    )
+  }
+
+  await prisma.questionPack.delete({
+    where: {
+      id: pack.id
+    }
+  })
+
+  return NextResponse.json({ message: 'Your question pack was deleted!' }, { status: 200 })
 }
--- utils/zod/schemas.ts
+++ utils/zod/schemas.ts
@@ -10,6 +10,22 @@ export const userSchema = z.object({
   likedPackPrivacy: z.boolean().optional(),
 })
 
+export const editedPackSchema = z.object({
+  name: z.string().min(4, 'Make sure your packs name is atleast 4 characters long').max(100, "Make sure your packs name is only 100 characters long"),
+  description: z.string().min(10, "Make sure your packs description is atleast 10 characters long").max(500, "Make sure your packs description is only 500 characters long"),
+  tags: z.array(z.string()).min(1, 'At least one tag is required').max(10),
+  questions: z.array(z.object({
+    question: z.string().min(10, "Make sure your question is atleast 10 characters long").max(100, "Make sure your question is only 100 characters long"),
+    type: z.enum(
+      ['truth', 'dare', 'wouldyourather', 'neverhaveiever', 'whatwouldyoudo', 'topic'],
+      {
+        required_error: 'Please select a valid question type',
+        message: 'Please select a valid question type'
+      }
+    )
+  })).min(1, 'At least one question is required').max(150, 'You can only have 100 questions in a pack')
+})
+
 export const packSchema = z.object({
   type: z.enum(
     [
