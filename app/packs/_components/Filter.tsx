df6304d60ffb1da2b63b90b866d062efe48ace5e
Woofer21 <61364477+Woofer21@users.noreply.github.com>
2024-12-18 15:43:04 -0500

    chore(packs): remove unused import

--- app/packs/_components/Filter.tsx
+++ app/packs/_components/Filter.tsx
@@ -4,7 +4,6 @@ import { Hash, Plus, Search } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { cn } from '@/lib/utils'
-import { useState } from 'react'
 import Link from 'next/link'
 
 const packTypes = [
